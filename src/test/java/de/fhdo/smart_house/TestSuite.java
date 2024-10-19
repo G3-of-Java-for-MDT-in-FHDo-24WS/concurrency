358d6450fec6334a558742d91c881068821510b1
linxin4cs <linxin4cs@gmail.com>
2024-10-19 23:39:28 +0200

    test: add tests into test suite

--- src/test/java/de/fhdo/smart_house/TestSuite.java
+++ src/test/java/de/fhdo/smart_house/TestSuite.java
@@ -4,13 +4,18 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 
+import de.fhdo.smart_house.service.CustomExceptionHandlerTest;
+import de.fhdo.smart_house.service.DirectoryManagerTest;
+import de.fhdo.smart_house.service.LogManageServiceTest;
 import de.fhdo.smart_house.service.LogSearchServiceTest;
 
 @Suite
 @SuiteDisplayName("My Test Suite")
 @SelectClasses({
-    LogSearchServiceTest.class
-   
+    LogSearchServiceTest.class,
+    LogManageServiceTest.class,
+    DirectoryManagerTest.class,
+    CustomExceptionHandlerTest.class
 })
 
 public class TestSuite {
--- src/test/java/de/fhdo/smart_house/service/CustomExceptionHandlerTest.java
+++ src/test/java/de/fhdo/smart_house/service/CustomExceptionHandlerTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
-class CustomExceptionHandlerTest {
+public class CustomExceptionHandlerTest {
 
     @Test
     void testHandleSingleException() {
